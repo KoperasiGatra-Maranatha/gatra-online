fc93e735f41e7a5895ed3bbe76357be29e0952e9
KoperasiGatra-Maranatha <awjeffryk@gmail.com>
2026-08-02 21:44:20 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'gatra-cache-v1';
+const assets = [
+  './Index.html',
+  './manifest.json',
+  'https://googleusercontent.com'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
